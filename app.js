398c9ec9a835094d1b036280fac377768446a685
Mapelujo <olamideiyanda18@gmail.com>
2023-11-21 21:25:21 +0100

    progress

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 console.log('Hello World');
 
 // Elements
+const selectClose = document.querySelector('.select-close');
+
+
+// Event Listerners
+selectClose.addEventListener('click', function(e){
+    e.preventDefault();
+    e.target.classlist.add('hidden-half')
+})
