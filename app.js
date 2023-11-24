1e0d338061cce8b7375edd71efd92c73116d0246
Mapelujo <olamideiyanda18@gmail.com>
2023-11-24 01:40:12 +0100

    progress

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log('Hello World');
 // Elements
 const selectClose = document.querySelector('.select-close');
 const selectAPlanContainer = document.querySelector('.select-a-plan');
+const setupDropdown = document.querySelector('#setup-dropdown');
 
 
 // Event Listerners
@@ -11,4 +12,9 @@ selectClose.addEventListener('click', function(e){
     e.preventDefault();
     e.target.closest('.select-a-plan').classList.add('invisible');
     // console.log('Clicked');
+})
+
+setupDropdown.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('Clicked');
 })
