928f3b1485ff9d7eb956f85132907dd0d552ff22
Mapelujo <olamideiyanda18@gmail.com>
2023-11-24 02:11:58 +0100

    progress

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log('Hello World');
 // Elements
 const selectClose = document.querySelector('.select-close');
 const selectAPlanContainer = document.querySelector('.select-a-plan');
-const setupDropdown = document.querySelector('#setup-dropdown');
+const setupDropdowns = document.querySelectorAll('#setup-dropdown');
 
 
 // Event Listerners
@@ -14,7 +14,13 @@ selectClose.addEventListener('click', function(e){
     // console.log('Clicked');
 })
 
-setupDropdown.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('Clicked');
-})
+setupDropdowns.forEach(function(setupDropdown){
+    setupDropdown.addEventListener('click', function(e){
+        e.preventDefault();
+        // console.log('Clicked');
+        if (e.target.classList.contains('setup-dropdown')) {
+            e.target.closest('.setup-dropdown').querySelector('.setup-dropdown-content').classList.toggle('invisible');
+            
+        }
+    })
+});
