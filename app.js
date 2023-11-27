4c78e502a774c5a5c5d3f5b4b6394f4fb6d2c7b4
Mapelujo <olamideiyanda18@gmail.com>
2023-11-27 11:08:01 +0100

    progress

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const selectClose = document.querySelector('.select-close');
 const selectAPlanContainer = document.querySelector('.select-a-plan');
 const setupDropdowns = document.querySelectorAll('#setup-dropdown');
 const setupAccordion = document.querySelector('.setup-div-flex-right');
-const accordion = document.querySelector('.accordion');
+const accordionTop = document.querySelector('.accordion-top');
+const accordionBottom = document.querySelector('.accordion-bottom');
 const setupContent = document.querySelector('.setup-content-flex');
 
 // Event Listerners
@@ -32,8 +33,7 @@ setupDropdowns.forEach(function (setupDropdown) {
 setupAccordion.addEventListener('click', function (e) {
   e.preventDefault();
   setupContent.classList.toggle('hidden');
-  accordion.setAttribute(
-    'd',
-    'M15.0303 12.2803C14.7374 12.5732 14.2626 12.5732 13.9697 12.2803L10.5 8.81066L7.03033 12.2803C6.73744 12.5732 6.26256 12.5732 5.96967 12.2803C5.67678 11.9874 5.67678 11.5126 5.96967 11.2197L9.96967 7.21967C10.2626 6.92678 10.7374 6.92678 11.0303 7.21967L15.0303 11.2197C15.3232 11.5126 15.3232 11.9874 15.0303 12.2803Z'
-  );
+  accordionTop.classList.toggle('hidden');
+  accordionBottom.classList.toggle('hidden');
+
 });
